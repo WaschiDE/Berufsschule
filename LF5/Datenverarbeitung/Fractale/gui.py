e7a3b6fe42e2fa1f267a555f33d2bf266f7d1b02
WaschbaerDE <44826829+WaschbaerDE@users.noreply.github.com>
2021-06-28 19:42:51 +0200

    sierpinskiTeppich.py fertig gestellt

---
+++ LF5/Datenverarbeitung/Fractale/gui.py
@@ -0,0 +1,42 @@
+import sierpinskiTeppich
+from tkinter import *
+from tkinter import ttk
+
+
+class Application(Frame):
+    def __init__(self, master=None):
+        master.title("Frakal-Writer")
+        Frame.__init__(self, master)
+
+        # Label
+        self.startzahl = Label(master, text="Welche Franktalart soll erstellt werden?"). \
+            grid(row=0, sticky=E, padx=10)
+        self.startsystem = Label(master, text="Welche Stufe soll erstellt werden?"). \
+            grid(row=1, sticky=E, padx=10)
+
+        self.stufe = Entry(master, width=50)
+        self.ouputText = Entry(master, state="disabled", width=50)
+
+        self.ouputText.grid(row=2, column=0,columnspan=2)
+
+        self.franktalart = ttk.Combobox(root, values=["Sierpinski-Teppich"])
+        self.franktalart.current(0)
+        self.franktalart.grid(row=0, column=1)
+
+        self.stufe = ttk.Combobox(root, values=["0", "1", "2", "3", "4", "5", "6"])
+        self.stufe.current(0)
+        self.stufe.grid(row=1, column=1)
+
+        Button(master, text='Datei Schreiben', width=20, command=self.action).grid(row=3, column=1, sticky=S, pady=4)
+
+    def action(self):
+        sierpinskiTeppich.main(self.stufe.get())
+
+        self.ouputText.delete(0, END)
+        self.ouputText.config(state="normal", background= "Green")
+        self.ouputText.insert(END, "Datei 'Sierpinksi_Teppich_Stufe_" + self.stufe.get() + ".svg' geschrieben!")
+
+# Mainfunktion zum Ausführen der GUI
+root = Tk()
+app = Application(master=root)
+app.mainloop()
---
+++ LF5/Datenverarbeitung/Fractale/sierpinskiTeppich.py
@@ -0,0 +1,37 @@
+def inCarpet(x,y):
+    while x!= 0 and y != 0:
+        if x % 3 == 1 and y % 3 == 1:
+            return "black"
+        x /= 3
+        y /= 3
+    return "white"
+def main(stufe):
+    stufe = int(stufe)
+    canvaseSize = 180
+    header = "<?xml version=\"1.0\"?>\n<svg xmlns=\"http://www.w3.org/2000/svg\">\n"
+    footer = "</svg>"
+    filename = "output/Sierpinksi_Teppich_Stufe_" + str(stufe) + ".svg"
+    file = open(filename, "w+")
+    file.write(header)
+
+
+    k = 0
+    while k <= stufe:
+        step = canvaseSize / pow(3, k)
+        fieldsize = canvaseSize / pow(3, k)
+        i = 0
+        while i < pow(3, k):
+            j = 0
+            while j < pow(3, k):
+                color = inCarpet(i, j)
+                if color == "black":
+                    line = "    <rect x=\"" + str(fieldsize * i) + "\" y=\"" + str(fieldsize * j) + "\" width=\"" + str(fieldsize) + "\" height=\"" + str(fieldsize) + "\" fill=\"" + color + "\"/>\n"
+                    file.write(line)
+                j += 1
+
+            i += 1
+        k += 1
+    file.write(footer)
+    file.close()
+    print("Sierpinksi_Teppich_Stufe_" + str(stufe) + ".svg generiert!")
+
